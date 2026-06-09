5198abdedc1f401d3d0b2ed224cac2ca318085ad
abhishektomarwins-stack <abhishektomarwins@gmail.com>
2026-06-09 08:58:04 +0530

    chnages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  optimizeDeps: {
+    include: [],
+  },
+  server: {
+    warmup: {
+      clientFiles: ['./index.html', './src/main.js', './src/input.css'],
+    },
+  },
 })
